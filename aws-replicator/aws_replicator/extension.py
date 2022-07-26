fece77896b0c3de82795095a285b202b4377a868
Waldemar Hummer <waldemar.hummer@gmail.com>
2022-07-26 19:34:21 +0200

    refactorings around use of Response attributes

--- aws-replicator/aws_replicator/extension.py
+++ aws-replicator/aws_replicator/extension.py
@@ -3,9 +3,6 @@ import logging
 from localstack.extensions.api import Extension, http
 from localstack.services.internal import get_internal_apis
 
-from aws_replicator.config import HANDLER_PATH
-from aws_replicator.server import RequestHandler
-
 LOG = logging.getLogger(__name__)
 
 
@@ -13,6 +10,9 @@ class AwsReplicatorExtension(Extension):
     name = "aws-replicator"
 
     def update_gateway_routes(self, router: http.Router[http.RouteHandler]):
+        from aws_replicator.config import HANDLER_PATH
+        from aws_replicator.server import RequestHandler
+
         LOG.info("AWS resource replicator: adding routes to activate extension")
         endpoint = RequestHandler()
-        get_internal_apis().add(HANDLER_PATH, endpoint)
+        get_internal_apis().add(HANDLER_PATH, endpoint, methods=["POST"])
