22a022a69de019981904c22fb328e166e53ffce3
Waldemar Hummer <waldemar.hummer@gmail.com>
2023-05-28 10:38:24 +0200

    return only bucket names from Lambda

--- aws-replicator/example/lambda.py
+++ aws-replicator/example/lambda.py
@@ -6,6 +6,7 @@ def handler(event, context):
     buckets = s3.list_buckets().get("Buckets")
     print("event:", event)
     print("buckets:", buckets)
+    bucket_names = [b["Name"] for b in buckets]
     return {
-        "buckets": buckets
+        "buckets": bucket_names
     }
