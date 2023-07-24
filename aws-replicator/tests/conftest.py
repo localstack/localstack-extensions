798644d93e381ce7405ca1bfccced87aa6a524db
sannya-singal <sannyasingal@gmail.com>
2023-07-24 13:03:09 +0530

    fix failing integration tests

--- aws-replicator/tests/conftest.py
+++ aws-replicator/tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from localstack.testing.aws.util import base_aws_client, base_aws_client_factory, base_aws_session
+from localstack.testing.aws.util import primary_testing_aws_client, base_aws_client_factory, base_aws_session
 
 pytest_plugins = [
     "localstack.testing.pytest.fixtures",
@@ -18,4 +18,4 @@ def aws_client_factory(aws_session):
 
 @pytest.fixture(scope="session")
 def aws_client(aws_client_factory):
-    return base_aws_client(aws_client_factory)
+    return primary_testing_aws_client(aws_client_factory)
