e6d348ce145b723c79d1b581aea948a771b9766f
Pive01 <lucapivetta01@gmail.com>
2024-07-03 14:52:33 +0200

    Added comment

--- template/react/{{cookiecutter.project_slug}}/{{cookiecutter.module_name}}/frontend/src/CustomRoutes.tsx
+++ template/react/{{cookiecutter.project_slug}}/{{cookiecutter.module_name}}/frontend/src/CustomRoutes.tsx
@@ -5,7 +5,8 @@ import { Dashboard } from "./Dashboard";
 
 export const CustomRoutes = (): ReactElement => (
   <Routes>
-    <Route path="/" element={<Navigate replace to="/dashboard" />} />
+    {/* Make sure to always have a route fallback to "/" as it is the default page that gets opened from the web app*/}
+    <Route path="/" element={<Navigate replace to="/dashboard" />} /> 
     <Route element={<Dashboard />} path="/dashboard" />
     <Route element={<PageOne />} path="/one" />
   </Routes>
