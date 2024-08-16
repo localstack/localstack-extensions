3ff9fab7729609dea95281de9a23b191a0fa6aea
Thomas Rausch <thomas@thrau.at>
2024-08-16 19:35:54 +0200

    introduce templates/ directory and add basic template (#78)
    Co-authored-by: Luca Pivetta <36865043+Pive01@users.noreply.github.com>

---
+++ templates/basic/{{cookiecutter.project_slug}}/{{cookiecutter.module_name}}/extension.py
@@ -0,0 +1,22 @@
+from localstack.extensions.api import Extension, http, aws
+
+class {{ cookiecutter.class_name }}(Extension):
+    name = "{{ cookiecutter.project_slug }}"
+
+    def on_extension_load(self):
+        print("MyExtension: extension is loaded")
+
+    def on_platform_start(self):
+        print("MyExtension: localstack is starting")
+
+    def on_platform_ready(self):
+        print("MyExtension: localstack is running")
+
+    def update_gateway_routes(self, router: http.Router[http.RouteHandler]):
+        pass
+
+    def update_request_handlers(self, handlers: aws.CompositeHandler):
+        pass
+
+    def update_response_handlers(self, handlers: aws.CompositeResponseHandler):
+        pass
