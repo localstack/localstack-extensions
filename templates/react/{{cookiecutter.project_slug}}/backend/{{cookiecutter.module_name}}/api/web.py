616870ebb2c284da6f5e4fd128b133b26301e593
Luca Pivetta <36865043+Pive01@users.noreply.github.com>
2025-06-26 16:12:03 +0200

    refactor(React template): Add easier setup using HashRoute (#97)

--- templates/react/{{cookiecutter.project_slug}}/backend/{{cookiecutter.module_name}}/api/web.py
+++ templates/react/{{cookiecutter.project_slug}}/backend/{{cookiecutter.module_name}}/api/web.py
@@ -1,12 +1,21 @@
-from localstack.http import route, Request, Response
+import logging
+
+from localstack.http import Request, Response, route
 
 from .. import static
+
+LOG = logging.getLogger(__name__)
+
 
 class WebApp:
     @route("/")
     def index(self, request: Request, *args, **kwargs):
         return Response.for_resource(static, "index.html")
-    
+
     @route("/<path:path>")
     def index2(self, request: Request, path: str, **kwargs):
-        return Response.for_resource(static, path)
+        try:
+            return Response.for_resource(static, path)
+        except Exception:
+            LOG.debug(f"File {path} not found, serving index.html")
+            return Response.for_resource(static, "index.html")
